ac03701077fff73647bf7d2baa3ea9b429f89be4
atillaturker <atillaturker2@gmail.com>
2025-04-07 23:29:33 +0300

    feat:favorites screen restyle

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -1,23 +1,34 @@
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
 import { fetchMovieDetails } from "@/services/api";
 import { FavoriteService } from "@/services/favoriteService";
 import { useMutation } from "@/services/useMutation";
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 import MovieCard from "../../components/movieCard";
 
 const Favorites = () => {
   const [favoriteIds, setFavoriteIds] = useState<string[]>([]);
   const [movies, setMovies] = useState<Movie[]>([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   const { mutate: loadFavorites } = useMutation(FavoriteService.getFavorites, {
     onSuccess: (ids) => {
       setFavoriteIds(ids);
-      loadMovies(ids); // ID'ler geldikten sonra filmleri yükle
+      loadMovies(ids);
     },
     onError: (error) => {
       console.error("Favori yükleme hatası:", error);
       setLoading(false);
+      setError(true);
     },
   });
 
@@ -51,18 +62,43 @@ const Favorites = () => {
   }
 
   return (
-    <View className="flex-1 p-4">
-      {movies.length > 0 ? (
-        <FlatList
-          data={movies}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => <MovieCard {...item} />}
-        />
-      ) : (
-        <View className="flex-1 justify-center items-center">
-          <Text className="text-lg">Henüz favori filminiz yok</Text>
-        </View>
-      )}
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView className="flex-1 px-5">
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        {loading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
+          />
+        ) : error ? (
+          <Text>Error: {error}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <>
+              <Text className="text-white text-lg font-bold mt-5 mb-3">
+                Favorite Movie
+              </Text>
+
+              <FlatList
+                data={movies}
+                renderItem={({ item }) => <MovieCard {...item} />}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  marginBottom: 10,
+                  paddingRight: 5,
+                }}
+                scrollEnabled={false}
+                className="mt-2 pb-32"
+              />
+            </>
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 };
